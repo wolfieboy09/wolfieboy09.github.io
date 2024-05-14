2db31569ffb1c8d51d826de2ee0b587bd96c28f7
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-05-14 08:35:07 -0400

    more done

---
+++ javascript/intro.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var intro = document.getElementById("intro");
+    intro.style.opacity = 1;
+    intro.style.transform = "translateY(0)";
+});
