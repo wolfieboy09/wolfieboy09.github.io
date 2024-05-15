cf1fbfd5fa3eb8223e5ccabd04c770e34fbb5355
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-05-15 07:13:04 -0400

    fixed null error

--- javascript/intro.js
+++ javascript/intro.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     var intro = document.getElementById("intro");
-    intro.style.opacity = 1;
-    intro.style.transform = "translateY(0)";
+    if (intro !== null) {
+        intro.style.opacity = 1;
+        intro.style.transform = "translateY(0)";
+    }
 });
